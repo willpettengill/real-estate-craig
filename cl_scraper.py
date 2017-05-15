f365e2f430379a60dc9b7465d9aa79b469aa9228
William Pettengill <Dewey@Sidney-Hook.local>
2017-05-15 11:00:23 -0400

    v1

---
+++ cl_scraper.py
@@ -0,0 +1,115 @@
+import pandas as pd
+import json
+import requests
+from bs4 import BeautifulSoup as bs4
+import craigslist_scraper
+from craigslist_scraper import scraper
+import numpy as np
+import psycopg2
+#dbname = scraper, table = re_data
+
+#TO DO:
+
+#Set up database functionality to write to AWS
+#create text file of all urls to prevent duplicate scraping
+#iterate to get all CL listings, rather than first 160
+#use: http://chrisholdgraf.com/querying-craigslist-with-python/
+#
+
+#Extend basic scraper function to include additional meta data from CL listing
+def AddMeta(x):
+	if x.soup:
+		
+		try:
+			x.address = x.soup.findAll(attrs={'class': 'mapaddress'})[0].text
+		except:
+			x.address = None	
+		try:	
+			x.latitude = float(re_data.soup.findAll(attrs={'class': 'mapbox'})[0].findChildren()[0].get('data-latitude'))
+		except:
+			x.latitude = None	
+		try:	
+			x.longitude = float(re_data.soup.findAll(attrs={'class': 'mapbox'})[0].findChildren()[0].get('data-longitude'))
+		except:
+			x.longitude = None
+		try:	
+			x.sqfeet = int(x.soup.findAll(attrs={'class': 'housing'})[0].text.replace('/','').replace('ft2','').replace('-','').strip())
+		except:
+			x.sqfeet = None
+		try:	
+			x.fulltitle = x.soup.findAll(attrs={'class': 'postingtitletext'})[0].text.replace(' \n\n\nhide this posting\n\n\n\n    unhide\n  \n\n','').replace('\n','')
+		except:
+			x.fulltitle = None
+		try:	
+			x.neighborhood = x.fulltitle[x.fulltitle.find('(')+1:x.fulltitle.find(')')]
+		except:
+			x.neighborhood = None
+	else:
+		pass
+
+#Parse a CL result page into a list of links to pass to scraper and AddMeta
+def getResults(page=1):
+	link_list = []
+	base_url = 'https://newyork.craigslist.org/search/off?bundleDuplicates=1'
+	#rsp = requests.get('https://newyork.craigslist.org/search/off?bundleDuplicates=1&min_price=1&max_price=1000000&minSqft=1&maxSqft=1000000&availabilityMode=0')
+	rsp = requests.get(base_url, params = {'min_price':1,'max_price':1000000, 'minSqft':1, 'maxSqft':1000000, 's':page})
+	html = bs4(rsp.text, 'html.parser')
+	listings = html.find_all(attrs={'class': 'result-row'})
+	for listing in listings:
+			detail =  listing.find_all(attrs={'class': 'result-title hdrlnk'})
+			link = 'https://newyork.craigslist.org/' + detail[0]['href']
+			link_list.append(link)
+	return link_list
+
+def DBbuild():
+	con = psycopg2.connect("dbname='scraper' user='wpettengill' password='wpettengill' host='craigslistdb.cnc0ky2ic2hk.us-east-1.rds.amazonaws.com' port='5432'")
+	cur = con.cursor()
+	#cur.execute('drop table if exists re_data; Create table re_data (address text, latitude double precision, longitude double precision, sqfeet int, fulltitle text, neighborhood text, price int, title text);')
+	return con, cur
+
+def DBwrite(re_data, con, cur):
+	try:
+		qry = '''
+		insert into re_data (address, latitude, longitude, sqfeet, fulltitle, neighborhood, price, title) values (%s, %s, %s, %s, %s, %s, %s, %s)
+		''' 
+		cur.execute(qry, (re_data.address, re_data.latitude, re_data.longitude, re_data.sqfeet, re_data.fulltitle, re_data.neighborhood, re_data.price, re_data.title))
+		print 'insert success'
+		con.commit()
+	except:
+		con.rollback()
+
+def checkdb():
+	qry = 'select * from re_data'
+	cur.execute(qry, con)
+	
+	for record in cur:
+		print record
+
+#def main():
+url_list = json.load(open('cl_listings.json'))
+con, cur = DBbuild()
+
+for i in np.arange(0,2500,100):
+	print 'index is %s' % i
+	urls = getResults(i)
+	for url in urls[:2]:
+		if url not in url_list or url in url_list:
+			
+			print url
+			url_list.append(url)
+			re_data = scraper.scrape_url(url)
+			AddMeta(re_data)
+			print re_data.price
+			print re_data.sqfeet
+			print re_data.neighborhood
+			print re_data.longitude
+			print re_data.latitude
+
+			DBwrite(re_data, con, cur) #Build this function
+
+	with open('cl_listings.json', 'w') as f:
+	        json.dump(list(set(url_list)), f)	
+
+
+if __name__ == "__main__":
+	main()
