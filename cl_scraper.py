9373dc97f558e1a895911c71700063565339651c
William Pettengill <Dewey@Sidney-Hook.hsd1.ma.comcast.net>
2017-05-17 16:54:09 -0400

    bugfix

--- cl_scraper.py
+++ cl_scraper.py
@@ -88,40 +88,38 @@ def checkdb():
 
 	print '%s total records'.format(len(cur))	
 
-#def main():
-url_list = json.load(open('cl_listings.json'))
-con, cur = DBbuild()
-#for i in np.arange(0,2500,100):
-i = 50
-
-urls = getResults(i)
-for url in urls:
-	print 'index is %s' % (i)
-	if url not in url_list:
-		url_list.append(url)
-		try:
-			re_data = scraper.scrape_url(url)
-		except:
-			print 'problem scraping'
-			print 'url is %s' % (url)
-			continue
-		try:
-			AddMeta(re_data)
-		except:
-			print 'problem adding additional meta'
-			print 'url is %s' % (url)
-			continue
-		try:
-			DBwrite(re_data, con, cur) #Build this function
-		except:
-			print 'problem writing to db'
-			print 'url is %s' % (url)
-			continue
-		time.sleep(2)
-	else:
-		continue	
-	with open('cl_listings.json', 'w') as f:
-	        json.dump(list(set(url_list)), f)	
+def main():
+	url_list = json.load(open('cl_listings.json'))
+	con, cur = DBbuild()
+	for i in np.arange(0,2500,100):
+		urls = getResults(i)
+		for url in urls:
+			print 'index is %s' % (i)
+			if url not in url_list:
+				url_list.append(url)
+				try:
+					re_data = scraper.scrape_url(url)
+				except:
+					print 'problem scraping'
+					print 'url is %s' % (url)
+					continue
+				try:
+					AddMeta(re_data)
+				except:
+					print 'problem adding additional meta'
+					print 'url is %s' % (url)
+					continue
+				try:
+					DBwrite(re_data, con, cur) #Build this function
+				except:
+					print 'problem writing to db'
+					print 'url is %s' % (url)
+					continue
+				time.sleep(2)
+			else:
+				continue	
+		with open('cl_listings.json', 'w') as f:
+		        json.dump(list(set(url_list)), f)	
 
 
 if __name__ == "__main__":
